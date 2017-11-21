ba756ea9ebc2643676eb451805a3023087fef38f
pptaszek <p.ptaszek@dodax.com>
2017-11-21 22:51:15 +0100

    poprawki

--- Dzialki/src/main/java/pl/ptaszek/opole/model/Adres.java
+++ Dzialki/src/main/java/pl/ptaszek/opole/model/Adres.java
@@ -1,55 +1,64 @@
 package pl.ptaszek.opole.model;
 
 public class Adres {
-		
-		private String ulica;
-		
-		private String numerDomu;
-		
-		private String kodPocztowy;
-		
-		private String miejscowosc;
-	
-		public Adres() {}
 
-		public Adres(String ulica, String numerDomu, String kodPocztowy, String miejscowosc) {
-			this.ulica = ulica;
-			this.numerDomu = numerDomu;
-			this.kodPocztowy = kodPocztowy;
-			this.miejscowosc = miejscowosc;
-		}
+	private String ulica;
 
-		public String getUlica() {
-			return ulica;
-		}
+	private String numerDomu;
 
-		public void setUlica(String ulica) {
-			this.ulica = ulica;
-		}
+	private String kodPocztowy;
 
-		public String getNumerDomu() {
-			return numerDomu;
-		}
+	private String miejscowosc;
 
-		public void setNumerDomu(String numerDomu) {
-			this.numerDomu = numerDomu;
-		}
-
-		public String getKodPocztowy() {
-			return kodPocztowy;
-		}
-
-		public void setKodPocztowy(String kodPocztowy) {
-			this.kodPocztowy = kodPocztowy;
-		}
-
-		public String getMiejscowosc() {
-			return miejscowosc;
-		}
-
-		public void setMiejscowosc(String miejscowosc) {
-			this.miejscowosc = miejscowosc;
-		}
-
+	public Adres() {
 	}
 
+	public Adres(String ulica, String numerDomu, String kodPocztowy, String miejscowosc) {
+		this.ulica = ulica;
+		this.numerDomu = numerDomu;
+		this.kodPocztowy = kodPocztowy;
+		this.miejscowosc = miejscowosc;
+	}
+
+	public String getUlica() {
+		return ulica;
+	}
+
+	public void setUlica(String ulica) {
+		this.ulica = ulica;
+	}
+
+	public String getNumerDomu() {
+		return numerDomu;
+	}
+
+	public void setNumerDomu(String numerDomu) {
+		this.numerDomu = numerDomu;
+	}
+
+	public String getKodPocztowy() {
+		return kodPocztowy;
+	}
+
+	public void setKodPocztowy(String kodPocztowy) {
+		this.kodPocztowy = kodPocztowy;
+	}
+
+	public String getMiejscowosc() {
+		return miejscowosc;
+	}
+
+	public void setMiejscowosc(String miejscowosc) {
+		this.miejscowosc = miejscowosc;
+	}
+
+	/**
+	 * Zwraca tekstowa reprezentacje obiektu.
+	 * 
+	 * @return teksowy opis obiektu
+	 */
+	public String toString() {
+		return "Ulica: " + ulica + ", numer domu: " + numerDomu + ", kod pocztowy: " + kodPocztowy + ", miejscowosc:"
+				+ miejscowosc;
+	}
+}
